00481dcf68e9e049b24c3787b0b172901db25ca2
moxley <matthew.oxley@imagini.net>
2016-10-21 16:50:13 +0100

    fix bug

--- src/index.js
+++ src/index.js
@@ -294,7 +294,7 @@ class ReactTooltip extends Component {
 
     const originTooltip = e.currentTarget.getAttribute('data-tip')
     const isMultiline = e.currentTarget.getAttribute('data-multiline') || multiline || false
-    const placeholder = getTipContent(originTooltip, children, null, isMultiline)
+    const placeholder = getTipContent(originTooltip, children, undefined, isMultiline)
 
     if (isEmptyTip || disable) return // if the tooltip is empty, disable the tooltip
     const updateState = () => {
